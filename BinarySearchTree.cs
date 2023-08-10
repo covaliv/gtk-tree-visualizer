3a5400108fed25827a2aebe63e27682bf04de932
Vlad Covali <covali.vl@gmail.com>
2023-08-10 15:05:51 +0300

    Add comments for clarity

--- BinarySearchTree.cs
+++ BinarySearchTree.cs
@@ -1,14 +1,21 @@
 using System.Text;
 
+// This class represents a Binary Search Tree (BST), a tree data structure in which each node has at most two children, referred to as the left child and the right child.
 public class BinarySearchTree<T> : Tree<T> where T : IComparable<T>
 {
+    // This property holds a log of updates made during BST operations.
     public StringBuilder updatesLog { get; private set; } = new StringBuilder();
+    // This class represents a node in the BST.
     public class Node : Tree<T>.Node
     {
+        // This property holds the value of the node.
         public T Value { get; set; }
+
+        // These properties point to the left and right child nodes.
         public Node? Left { get; set; }
         public Node? Right { get; set; }
 
+        // These properties provide an interface for the base Node class's Left and Right properties.
         Tree<T>.Node? Tree<T>.Node.Left
         {
             get => Left;
@@ -19,6 +26,7 @@ public class BinarySearchTree<T> : Tree<T> where T : IComparable<T>
             get => Right;
         }
 
+        // This constructor initializes a new node with a given value and sets both child nodes to null.
         public Node(T value)
         {
             Value = value;
@@ -27,16 +35,20 @@ public class BinarySearchTree<T> : Tree<T> where T : IComparable<T>
         }
     }
 
+    // This property points to the root node of the tree.
     private Node? root;
 
+    // This property provides a getter for the root node, for use by the base Tree class.
     Tree<T>.Node? Tree<T>.Root => root;
 
+    // This property provides a getter and private setter for the root node, for use within this class.
     public Node? Root
     {
         get { return root; }
         private set { root = value; }
     }
 
+    // This method inserts a new value into the tree.
     public void Insert(T value)
     {
         updatesLog = new StringBuilder();
@@ -44,6 +56,7 @@ public class BinarySearchTree<T> : Tree<T> where T : IComparable<T>
         updatesLog.AppendLine($"Inserted {value} into the tree.");
     }
 
+    // This helper method recursively inserts a new value into the tree, using the BST property that all left descendants are less than the node and all right descendants are greater.
     private Node Insert(Node? node, T value)
     {
         if (node == null)
@@ -66,6 +79,7 @@ public class BinarySearchTree<T> : Tree<T> where T : IComparable<T>
         return node;
     }
 
+    // This method deletes a value from the tree.
     public void Delete(T value)
     {
         updatesLog = new StringBuilder();
@@ -73,6 +87,7 @@ public class BinarySearchTree<T> : Tree<T> where T : IComparable<T>
         updatesLog.AppendLine($"Deleted {value} from the tree.");
     }
 
+    // This helper method recursively deletes a value from the tree, preserving the BST property.
     private Node? Delete(Node? node, T value)
     {
         if (node == null)
@@ -115,6 +130,7 @@ public class BinarySearchTree<T> : Tree<T> where T : IComparable<T>
         return node;
     }
 
+    // This helper method finds the minimum value in the subtree rooted at a given node.
     private T MinValue(Node node)
     {
         T minValue = node.Value;
